d3ae985a09af12da6f499212fd1a667db37b6e98
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-06-26 15:42:17 -0400

    Initial Commit

---
+++ SpeedTest/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  SpeedTest
+//
+//  Created by Jacob O'Donnell on 6/26/12.
+//  Copyright Cyrus Innovation 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
